8a267ad5db09bf05da5b7b2257c490373e02133a
loganhansel <loganhansel23@gmail.com>
2022-08-10 10:56:32 -0400

    completed  function, which should now fetch an author from the API. It calls another function, , which is now initialized but not written.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.querySelector("search-form").addEventListener('submit', fetchAuthors())
 
 function fetchAuthors(event) {
+    event.preventDefault()
+    fetch(`https://openlibrary.org/search/authors.json?q=${event.target.name.value}`)
+    .then(response => response.json())
+    .then(appendAuthors(data))
+}
 
+function appendAuthors(authors) {
+    
 }
