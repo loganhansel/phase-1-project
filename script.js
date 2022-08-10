9218301f293e928036cb654c7c397b27cbdbf617
loganhansel <loganhansel23@gmail.com>
2022-08-10 16:30:32 -0400

    began creating the  function to append all the search results to the webpage. So far it prints the names of each result.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.getElementById("search-form").addEventListener('submit', fetchAuthors)
 // FETCH AUTHOR LIST FROM API
 function fetchAuthors(event) {
     event.preventDefault()
+    document.getElementById("search-results").innerHTML = ""
     let queryString = parseName(event.target.name.value)
     fetch(`https://openlibrary.org/search/authors.json?q=${queryString}`)
     .then(response => response.json())
@@ -13,13 +14,18 @@ function fetchAuthors(event) {
         let authorData = data["docs"]
         authorData.forEach(element => appendAuthors(element))
     })
+    document.getElementById("search-form").reset()
 }
 
 
 
 // APPENDS AUTHOR SEARCH RESULTS TO WEBPAGE
 function appendAuthors(authorData) {
-
+    let authorItem = document.createElement('li')
+    let authorName = document.createElement('h3')
+        authorName.innerText = authorData["name"]
+        authorItem.appendChild(authorName)
+    document.getElementById("search-results").appendChild(authorItem)
 }
 
 
