0a28482210a2eca4c0ccc51596073a95013c49bb
loganhansel <loganhansel23@gmail.com>
2022-08-10 16:02:01 -0400

    completed  function; uses  function to create a usable query string.  is now correctly pulling author search results from API. Array of results is ready to be iterated through.

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+// EVENT-LISTENERS
 document.getElementById("search-form").addEventListener('submit', fetchAuthors)
 
 
+
+// FETCH AUTHOR LIST FROM API
 function fetchAuthors(event) {
     event.preventDefault()
-    let nameValue = event.target.name.value
-    fetch(`https://openlibrary.org/search/authors.json?q=${parseName(nameValue)}`)
+    let queryString = parseName(event.target.name.value)
+    fetch(`https://openlibrary.org/search/authors.json?q=${queryString}`)
     .then(response => response.json())
-    .then(appendAuthors(data))
+    .then(function(data){
+        let authorData = data["docs"]
+        authorData.forEach(element => appendAuthors(element))
+    })
 }
 
-function appendAuthors(authors) {
+
+
+// APPENDS AUTHOR SEARCH RESULTS TO WEBPAGE
+function appendAuthors(authorData) {
 
 }
 
+
+
+// CONVERTS USER SEARCH INTO QUERY STRING
 function parseName(name) {
-    
+    let parsedName = encodeURIComponent(name)
+    return parsedName
 }
