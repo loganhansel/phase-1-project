55a5c663e797e054bc59c04295d9c47383a9cf54
loganhansel <loganhansel23@gmail.com>
2022-08-10 12:15:43 -0400

    created a function  that is going to accep the input from the user in the <form> and turn it into a readable query parameter for the API to read.

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ document.getElementById("search-form").addEventListener('submit', fetchAuthors)
 
 function fetchAuthors(event) {
     event.preventDefault()
-    fetch(`https://openlibrary.org/search/authors.json?q=${event.target.name.value}`)
+    let nameValue = event.target.name.value
+    fetch(`https://openlibrary.org/search/authors.json?q=${parseName(nameValue)}`)
     .then(response => response.json())
     .then(appendAuthors(data))
 }
 
 function appendAuthors(authors) {
 
+}
+
+function parseName(name) {
+    
 }
